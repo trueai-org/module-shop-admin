fca30d679f0992e2b3f850192771311c5ff41b2c
zhangkp <wood@trueai.org>
2019-07-06 20:19:40 +0800

    build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
 
+imgname="shop-web"
+imgport=6002
+imgversion="1.0.0"
 
-docker build -t shop-web-nginx:dev-1.0.0-beta2 .
+docker stop $imgname
+docker rm $imgname
+docker rmi $imgname:$imgname
 
-docker run -dit --name shop-web-nginx -p 6002:80 --restart=always shop-web-nginx:dev-1.0.0-beta2
+docker build -t $imgname:$imgversion .
+
+docker run -d --name $imgname -p $imgport:80 --restart=always $imgname:$imgversion
+
+docker logs $imgname
